f6960a79f481bbe53625340518d88944b7434da7
Min Sohng <minsohng@gmail.com>
2020-04-13 17:19:06 -0700

    update README, WIP: implement draw rectangles

--- client/scripts/helpers/buttons.js
+++ client/scripts/helpers/buttons.js
@@ -1,9 +1,9 @@
 import cv from './canvas.js'
-import { freehand, straightLine } from '../actions/index.js'
-
+import { freehand, straightLine, rectangles } from '../actions/index.js'
 
 const freehandBtn = document.getElementById('freehand')
 const lineBtn = document.getElementById('straightLine')
+const rectanglesBtn = document.getElementById('rectangles')
 
 freehandBtn.addEventListener('click', () => {
   removeOtherEventListeners('freehand')
@@ -19,11 +19,18 @@ lineBtn.addEventListener('click', () => {
   cv.canvas.addEventListener('mouseup', straightLine.mouseup)
 })
 
+rectanglesBtn.addEventListener('click', () => {
+  removeOtherEventListeners('rectangles')
+  cv.canvas.addEventListener('mousedown', rectangles.mousedown)
+  cv.canvas.addEventListener('mousemove', rectangles.mousemove)
+  cv.canvas.addEventListener('mouseup', rectangles.mouseup)
+})
+
 const removeOtherEventListeners = (btnId) => {
   const eventObj = {
     freehand,
     straightLine,
-    //rectangles
+    rectangles
   }
   for (let i in eventObj) {
     if (eventObj[i] !== btnId) {
